a6cedb0803592cb839506eb6e48bc910b6e02b5b
Dominik Boller <boller.dominik@gmail.com>
2022-05-09 14:41:11 +0200

    Update ExampleResource.java

--- src/main/java/com/example/ExampleResource.java
+++ src/main/java/com/example/ExampleResource.java
@@ -1,16 +1,20 @@
 package com.example;
 
-import io.quarkus.vertx.ConsumeEvent;
-import io.smallrye.mutiny.Uni;
-import io.vertx.core.eventbus.EventBus;
+import java.time.Duration;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.time.Duration;
+
+import io.quarkus.vertx.LocalEventBusCodec;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.subscription.UniEmitter;
+import io.vertx.mutiny.core.eventbus.EventBus;
+import io.vertx.mutiny.core.eventbus.MessageConsumer;
 
 @Path("/test")
 public class ExampleResource {
@@ -18,29 +22,37 @@ public class ExampleResource {
     @Inject
     EventBus eventBus;
 
+    @PostConstruct
+    public void init() {
+        eventBus.getDelegate().registerDefaultCodec(EventData.class, new LocalEventBusCodec<>());
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/waitForEvent")
-    public Uni<String> waitForEvent() {
-
-
+    public Uni<EventData> waitForEvent(@QueryParam("eventId") String eventId) {
         return Uni.createFrom()
-                .<String>emitter(emitter -> {
-                    //todo what???
-                    // Wait for event from the GET /send endpoint
-                    // return message
-                })
-                .ifNoItem().after(Duration.ofSeconds(5)).failWith(new RuntimeException("timeout"));
+                .<EventData>emitter(uniEmitter -> setupEmitter(eventId, uniEmitter))
+                .ifNoItem().after(Duration.ofSeconds(5))
+                .failWith(new RuntimeException("timeout"));
     }
 
     @GET
     @Path("/send")
-    public void test(@QueryParam("msg") String msg) {
-        eventBus.send("test", msg);
+    public void test(@QueryParam("msg") String msg, @QueryParam("eventId") String eventId) {
+        eventBus.publish("authorizationEvent", new EventData(eventId, msg));
     }
 
-    @ConsumeEvent("test")
-    public void consumeEvent(String msg) {
-        System.out.println("received: " + msg);
+    private void setupEmitter(String eventId, UniEmitter<? super EventData> uniEmitter) {
+        MessageConsumer<EventData> consumer = eventBus.consumer("authorizationEvent");
+        consumer.handler(m -> {
+            System.out.println("Received: " + m.body());
+
+            if (m.body().getEventId().equals(eventId)) {
+                uniEmitter.complete(m.body());
+                consumer.unregisterAndForget();
+            }
+
+        });
     }
 }
